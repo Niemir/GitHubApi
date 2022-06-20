46ccd0b13a429f5edea77601ab4f69b6c514741d
marcinniemirski <91262813+marcinniemirski@users.noreply.github.com>
2022-06-20 22:28:02 +0200

    add react router

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../assets/styles/globalStyle";
 import { theme } from "../assets/styles/theme";
@@ -17,12 +18,14 @@ const queryClient = new QueryClient({
 
 const AppProvider: FC<AppProviderProps> = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient} contextSharing={true}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        {children}
-      </ThemeProvider>
-    </QueryClientProvider>
+    <Router>
+      <QueryClientProvider client={queryClient} contextSharing={true}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          {children}
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Router>
   );
 };
 
